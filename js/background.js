aaef051394202651b540176ff495b14e210cd1c3
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2022-10-15 21:45:00 +0900

    배경이미지 추가

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
-const images = ['1.jpeg', '2.jpeg'];
+const images = ["0.webp", "1.jpeg", "2.jpeg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.querySelector('.background');
+const bgImage = document.querySelector(".background");
 bgImage.style.backgroundImage = `url(img/${chosenImage})`;
