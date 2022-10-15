01c4cb61458aba512089c62f62cc1307f272473d
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2022-10-15 22:33:07 +0900

    logout기능 추가

--- js/greetings.js
+++ js/greetings.js
@@ -1,31 +1,45 @@
-const loginForm = document.querySelector('.login-form');
-const loginInput = document.querySelector('.login-form input');
-const greeting = document.querySelector('#greeting');
-const todo = document.querySelector('.todo_list');
+const loginForm = document.querySelector(".login-form");
+const logoutForm = document.querySelector(".logout_form");
+const loginInput = document.querySelector(".login-form input");
+const greeting = document.querySelector("#greeting");
+const todo = document.querySelector(".todo_list");
 
-const HIDDEN_CLASSNAME = 'hidden';
-const USERNAME_KEY = 'username';
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
-    todo.value = '';
-    todo.focus();
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintLogout();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+  todo.value = "";
+  todo.focus();
+}
+
+function onLogoutSubmit() {
+  localStorage.clear();
+  logoutForm.classList.add(HIDDEN_CLASSNAME);
 }
 
 function paintGreetings(username) {
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `${username}'s To Do List`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `${username}'s To Do List`;
+}
+
+function paintLogout() {
+  logoutForm.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener('submit', onLoginSubmit);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreetings(savedUsername);
+  paintGreetings(savedUsername);
+  paintLogout();
 }
+
+logoutForm.addEventListener("submit", onLogoutSubmit);
