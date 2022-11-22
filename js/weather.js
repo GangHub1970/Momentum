edc5a3faa56fe25fbdcd157998c4cfac152f09e0
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2022-11-23 00:01:34 +0900

    .gitignore 수정

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,23 @@
-const API_KEY = 'faf4215147954724d5af374f5b530454';
+const WEATHER_API_KEY = config.weather_apikey;
 
 function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const weather = document.querySelector('.weather span:first-child');
-            const city = document.querySelector('.weather span:last-child');
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp.toFixed(1)}°C`;
-            city.innerText = data.name;
-    })
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector(".weather span:first-child");
+      const city = document.querySelector(".weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp.toFixed(
+        1
+      )}°C`;
+      city.innerText = data.name;
+    });
 }
 
 function onGeoError() {
-    alert("Can't find you. No weather for you.");
+  alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
